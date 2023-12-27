8df614f7bdda012a20ff977a7e4e9fed7b891e8f
Dimi <sztanko@gmail.com>
2023-12-27 21:10:34 +0000

    Fix baseURI in the document

--- app/elucidario/src/pages/_document.tsx
+++ app/elucidario/src/pages/_document.tsx
@@ -2,17 +2,18 @@ import { Html, Head, Main, NextScript } from 'next/document'
 import i18nextConfig from '../../next-i18next.config'
 
 export default function Document (props) {
-  console.log('props', props)
+  // console.log('props', props)
   const currentLocale =
     props.__NEXT_DATA__.query.locale || i18nextConfig.i18n.defaultLocale
-  console.info('currentLocale', currentLocale)
+
+  const baseURI = process.env.WEB_PATH || ''
   return (
     <Html lang={currentLocale}>
       <Head />
       <body
         style={{
           minHeight: '100vh',
-          background: `url('/background.webp'), linear-gradient(to right, rgb(232, 232, 220), rgb(230, 229, 218))`,
+          background: `url('${baseURI}/background.webp'), linear-gradient(to right, rgb(232, 232, 220), rgb(230, 229, 218))`,
           backgroundPosition: 'center bottom',
           backgroundRepeat: 'no-repeat',
           backgroundSize: '100% auto'
@@ -20,9 +21,6 @@ export default function Document (props) {
       >
         
         <Main />
-        <pre>
-          {props.__NEXT_DATA__.query.locale} || {i18nextConfig.i18n.defaultLocale}
-        </pre>
         <NextScript />
       </body>
     </Html>
