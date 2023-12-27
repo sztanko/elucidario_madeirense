513031973ee6681bbee78104fcc10bd0bf5bcc2d
Dimi <sztanko@gmail.com>
2023-12-27 15:01:40 +0000

    Change base path

--- app/elucidario/next.config.js
+++ app/elucidario/next.config.js
@@ -7,6 +7,8 @@ const nextConfig = {
     locales: ['en', 'pt', 'de', 'ru', 'ua'],
     defaultLocale: 'en'
   }*/
+  // Use WEB_PATH env variable
+  basePath: process.env.WEB_PATH || '',
 }
 
 module.exports = nextConfig
